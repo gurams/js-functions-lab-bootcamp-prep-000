c194d352a88c4f09293490f9f5cd922852d5c13e
Ramesh <gurams@gmail.com>
2017-10-16 02:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,3 @@ function happyHolidays(){
   var greet = "Happy holidays!";
   return  greet;
 }
-
-function happyHolidaysTo(name){
-  return  "Happy holidays, ${name}!"
-}
-
-function happyHolidaysTo(holiday, name){
-  return  "Happy ${holiday}, ${name}!"
-
-function holidayCountdown(holiday, days){
-  return  "It\'s ${days} days until ${holiday}!"
-}
